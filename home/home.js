b1a99894a14388f21b9e3f2712f60e35a0068ddf
Winnie207 <57951904+Winnie207@users.noreply.github.com>
2020-04-12 13:34:11 +0900

    グラデーションを採用

--- home/home.js
+++ home/home.js
@@ -3,20 +3,29 @@ $(function() {
 
   setTimeout(function() {
 
-    $.when(
-      $('#welcome').fadeIn('slow')
-    ).done(function() {
-      $('#scroll1').fadeIn('slow');
-    });
+    $('#bg-color').fadeIn('slow');
+    $('#welcome').fadeIn('slow');
+    $('#scroll1').fadeIn('slow');
 
   }, 2000);
 
 
   $('#scroll1').click(function() {
+
     $('html, body').animate({
-      // scrollTop: $('#q-wrapper').offset().top
-      scrollTop: '750vh'  // なぜ770vh？
-    }, 'slow');
+      scrollTop: $(this).offset().top + $(this).height()
+      // scrollTop: '750vh'  // なぜ770vh？
+    }, 'slow', 'easeOutQuart');
+
+  });
+
+
+  $('.scroll').click(function() {
+
+    $('html, body').animate({
+      scrollTop: $(this).offset().top + $(this).outerHeight(true)
+    }, 'slow', 'easeOutQuart');
+
   });
 
 
